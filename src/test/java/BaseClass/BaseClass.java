9ef39ab8c6650e85a5be64a0a484ad4668ce6fa2
Arunsk00 <arunsk199514@gmail.com>
2025-05-26 16:53:21 +0530

    Changes made

--- src/test/java/BaseClass/BaseClass.java
+++ src/test/java/BaseClass/BaseClass.java
@@ -26,6 +26,7 @@ public class BaseClass {
 	public static void baseURI(String baseURI) {
 		RestAssured.baseURI = baseURI;
 		given = RestAssured.given();
+		//Changed Successfully
 	}
 
 	public static void queryParameter(String parameterName, String parameterValue) {
